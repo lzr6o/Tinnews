2ce53e44f51d9782b710403029ea0d1d580fb559
李zr <zr6oli@gmail.com>
2022-08-08 17:34:02 -0400

    edit

--- app/src/main/java/com/laioffer/tinnews/model/Article.java
+++ app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -1,5 +1,7 @@
 package com.laioffer.tinnews.model;
 
+import java.util.Objects;
+
 public class Article {
     public String author;
     public String content;
@@ -8,4 +10,30 @@ public class Article {
     public String title;
     public String url;
     public String urlToImage;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(author, article.author) && Objects.equals(content, article.content) && Objects.equals(description, article.description) && Objects.equals(publishedAt, article.publishedAt) && Objects.equals(title, article.title) && Objects.equals(url, article.url) && Objects.equals(urlToImage, article.urlToImage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, content, description, publishedAt, title, url, urlToImage);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                ", description='" + description + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
+                '}';
+    }
 }
