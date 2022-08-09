aae86ec1f0bcac791641044130e6e0d7846cdd1d
李zr <zr6oli@gmail.com>
2022-08-09 14:25:28 -0400

    list view implement

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
 import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 public class HomeFragment extends Fragment {
 
@@ -35,7 +37,7 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         NewsRepository repository = new NewsRepository();
-        viewModel = new HomeViewModel(repository);
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(HomeViewModel.class);
         viewModel.setCountryInput("us");
         viewModel
                 .getTopHeadlines()
---
+++ app/src/main/java/com/laioffer/tinnews/ui/search/SearchNewsAdapter.java
@@ -0,0 +1,43 @@
+package com.laioffer.tinnews.ui.search;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.laioffer.tinnews.databinding.SearchNewsItemBinding;
+import com.laioffer.tinnews.model.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchNewsAdapter {
+    // 1. Supporting data:
+    private List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
+
+
+    // 2. SearchNewsViewHolder:
+    public static class SearchNewsViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView itemImageView;
+        TextView itemTitleTextView;
+
+        public SearchNewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            SearchNewsItemBinding binding = SearchNewsItemBinding.bind(itemView);
+            itemImageView = binding.searchItemImage;
+            itemTitleTextView = binding.searchItemTitle;
+        }
+    }
+
+    // 3. Adapter overrides:
+    // TODO
+}
